642d9b2635f4b01463e39820eeb2e656dcaab3ab
unknown <adamjsteinbacher@gmail.com>
2020-06-05 13:24:41 -0500

    initial commit

---
+++ DinosaursVsRobots/DinosaursVsRobots/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DinosaursVsRobots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
